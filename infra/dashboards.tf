4d83e1e07bc259f31d0bb5e78ce6b10d79c27063
Martin Teigen <martin@teigen.biz>
2022-12-11 19:18:57 +0100

    Add comma

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -70,7 +70,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "checkout_latency.avg"
+            "checkout_latency.avg",
             "none",
             "checkout",
             "no.shoppifly.ShoppingCartController"
